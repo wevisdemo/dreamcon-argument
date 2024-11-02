0319049df272bfa21b4154305284d5339b2bde18
petchill <songpon111333@gmail.com>
2024-11-02 23:56:50 +0700

    feat: connect with firebase database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+import { useEffect, useState } from "react";
+import { database, ref, onValue, set, push } from "../lib/firebase";
 export default function Home() {
+  const [data, setData] = useState<Room[]>([]);
+  useEffect(() => {
+    const dataRef = ref(database, "rooms/");
+    onValue(dataRef, (snapshot) => {
+      const dbValue = snapshot.val();
+      console.log(dbValue);
+      setData(dbValue);
+    });
+  }, []);
+
+  const onclick = async () => {
+    const dataRef = ref(database, "rooms/");
+    await push(dataRef, { data: "test" });
+  };
   return (
     <div className="">
       <h1 className="wv-h1 ">Hello world</h1>
+      <button onClick={onclick}>click</button>
     </div>
   );
 }
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,14 @@
 // lib/firebase.js
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue, set } from "firebase/database";
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
 
 // Firebase configuration - replace these values with your own
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-};
+const firebaseConfig = JSON.parse(
+  process.env.NEXT_PUBLIC_FIREBASE_CONFIG || "{}"
+);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-export { database, ref, onValue, set };
+export { database, ref, onValue, set, push };

5db8ba9e4077cad2a59e80bcf7c1e5d2ea6a573f
petchill <songpon111333@gmail.com>
2024-11-19 17:40:14 +0700

    feat: connect api in comment page

--- src/pages/comment.tsx
+++ src/pages/comment.tsx
@@ -1,87 +1,240 @@
 import CommentCard from "../components/commentCard";
-import Hero from "../components/hero";
-import TitleCard from "../components/rooms/titleCard";
-import { mockComment, mockPreviousComments, mockRoom } from "../data/room";
-import { CommentView } from "../types/room";
+import { CommentView, Comment, Room } from "../types/room";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  database,
+  get,
+  increment,
+  onValue,
+  ref,
+  update,
+} from "../lib/firebase";
+import { ConvertRoom, GetCommentWitChildren } from "../util/converter";
+
+interface MapComment {
+  [id: string]: Comment;
+}
 
 export default function CommentPage() {
   const { id } = useParams<{ id: string }>();
+  const [mapPreviousComment, setMapPreviousComment] = useState<MapComment>({});
+  const [room, setRoom] = useState<Room | null>(null);
+  const [comment, setComment] = useState<Comment | null>(null);
   const commentId = id as string;
 
-  const room = mockRoom; // TODO: change this to real data
-  const previouseComments = mockPreviousComments;
-  const comment = mockComment;
+  const previousComments = () => Object.values(mapPreviousComment);
+
+  useEffect(() => {
+    fetchAndWatchComment();
+  }, []);
+
+  useEffect(() => {
+    if (comment) {
+      fetchAndWatchRoom();
+      fetchAndWatchPreviousComments();
+    }
+  }, [comment]);
+
+  const fetchComment = async () => {
+    const commentRef = ref(database, "comments/" + commentId);
+    await get(commentRef).then(async (snapshot) => {
+      if (snapshot.exists()) {
+        const convertedComment = await GetCommentWitChildren(commentId);
+        if (convertedComment == null) {
+          return;
+        }
+        console.log("setComment => ", convertedComment);
+        setComment(convertedComment);
+      }
+    });
+  };
+
+  const fetchAndWatchComment = async () => {
+    const commentRef = ref(database, "comments/" + commentId);
+    onValue(commentRef, async (snapshot: any) => {
+      const commentData = snapshot.val();
+      if (commentData) {
+        const convertedComment = await GetCommentWitChildren(commentId);
+        if (convertedComment == null) {
+          return;
+        }
+        console.log("setComment => ", convertedComment);
+        setComment(convertedComment);
+      }
+    });
+    console.log("done comment => ", comment);
+  };
+
+  const fetchAndWatchRoom = async () => {
+    if (!comment) {
+      console.log("No comment 1");
+
+      return;
+    }
+    const roomRef = ref(database, "rooms/" + comment.parent_room_id);
+    onValue(roomRef, async (snapshot: any) => {
+      const roomData = snapshot.val();
+
+      if (roomData) {
+        const convertedRoom = await ConvertRoom(
+          comment.parent_room_id,
+          roomData
+        );
+        setRoom(convertedRoom);
+      }
+    });
+  };
+
+  const fetchAndWatchPreviousComments = async () => {
+    if (!comment) {
+      console.log("No comment 2");
+
+      return;
+    }
+    for (const id of comment.parent_comment_ids) {
+      const commentRef = ref(database, "comments/" + id);
+      onValue(commentRef, async (snapshot: any) => {
+        const comment = snapshot.val();
+        if (comment) {
+          const convertedComment = await GetCommentWitChildren(id);
+          if (convertedComment == null) {
+            return;
+          }
+
+          setMapPreviousComment((prev) => ({
+            ...prev,
+            [id]: convertedComment,
+          }));
+        }
+      });
+    }
+  };
 
   const getCommentsByView = (view: CommentView) => {
-    return comment.comments.filter((c) => c.comment_view === view);
+    return (comment?.comments || []).filter((c) => c.comment_view === view);
+  };
+
+  const handleLikeChildComment = (targetCommentId: string) => {
+    const dbRef = ref(database);
+    const updates = {
+      [`comments/${targetCommentId}/like_count`]: increment(1),
+    };
+    update(dbRef, updates);
+
+    fetchComment();
+  };
+
+  const handleLikeComment = () => {
+    if (!comment) {
+      return;
+    }
+    const dbRef = ref(database);
+    const updates = {
+      [`comments/${commentId}/like_count`]: increment(1),
+    };
+    update(dbRef, updates);
+  };
+
+  const handleLikePreviousComment = (targetCommentId: string) => {
+    const dbRef = ref(database);
+    const updates = {
+      [`comments/${targetCommentId}/like_count`]: increment(1),
+    };
+    update(dbRef, updates);
   };
 
   return (
-    <div className="flex flex-col">
-      <section
-        id="header"
-        className="px-[16px] md:px-[160px] py-[8px] md:py-[16px] bg-[#BDE6FF]"
-      >
-        <a
-          href="/"
-          className="wv-ibmplex wv-semibold text-[#1C4CD3] underline "
-        >
-          {"< ข้อถกเถียง"}
-        </a>
-      </section>
-      <section className="md:px-[160px] p-0">
-        <h4 className="wv-ibmplex wv-bold text-[16px] md:text-[25px] bg-[#FFFFFF] rounded-b-[8px] drop-shadow-md p-[16px] md:p-[24px]">
-          {room.title}
-        </h4>
-        {previouseComments.map((comment) => (
-          <div
-            className="mt-[40px] w-full"
-            key={`parent-comment-${comment.id}`}
+    <div>
+      {comment != null && room != null ? (
+        <div className="flex flex-col">
+          {comment != null && room != null ? <div /> : <div>Loading...</div>}
+          <section
+            id="header"
+            className="px-[16px] md:px-[160px] py-[8px] md:py-[16px] bg-[#BDE6FF]"
           >
-            <CommentCard
-              comment={comment}
-              fullWidth
-              onClickLike={() => {}}
-              onClickAddComment={() => {}}
-            />
-          </div>
-        ))}
-        <div className="grid grid-cols-1 md:grid-cols-3 mt-[16px]">
-          <div className=" w-full flex flex-col gap-[16px] items-center">
-            {getCommentsByView(CommentView.AGREE).map((comment, index) => (
-              <CommentCard
-                onClickLike={() => {}}
-                key={`agree-comment-${index}`}
-                comment={comment}
-                onClickAddComment={() => {}}
-              />
-            ))}
-          </div>
-          <div className="w-full flex flex-col gap-[16px] items-center">
-            {getCommentsByView(CommentView.PARTIAL_AGREE).map(
-              (comment, index) => (
+            <a
+              href="/"
+              className="wv-ibmplex wv-semibold text-[#1C4CD3] underline "
+            >
+              {"< ข้อถกเถียง"}
+            </a>
+          </section>
+          <section className="md:px-[160px] p-0">
+            <h4 className="wv-ibmplex wv-bold text-[16px] md:text-[25px] bg-[#FFFFFF] rounded-b-[8px] drop-shadow-md p-[16px] md:p-[24px]">
+              {room?.title}
+            </h4>
+            {previousComments().map((previousComment) => (
+              <div
+                className="mt-[40px] w-full"
+                key={`parent-comment-${previousComment.id}`}
+              >
                 <CommentCard
-                  onClickLike={() => {}}
-                  key={`partial-agree-comment-${index}`}
-                  comment={comment}
+                  comment={previousComment}
+                  fullWidth
+                  onClickLike={() => handleLikeChildComment(previousComment.id)}
                   onClickAddComment={() => {}}
                 />
-              )
-            )}
-          </div>
-          <div className="w-full flex flex-col gap-[16px] items-center">
-            {getCommentsByView(CommentView.DISAGREE).map((comment, index) => (
+              </div>
+            ))}
+            <div className="mt-[40px] w-full">
               <CommentCard
-                onClickLike={() => {}}
-                key={`disagree-comment-${index}`}
                 comment={comment}
+                fullWidth
+                onClickLike={() => handleLikeComment()}
                 onClickAddComment={() => {}}
               />
-            ))}
-          </div>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 mt-[16px]">
+              <div className=" w-full flex flex-col gap-[16px] items-center">
+                {getCommentsByView(CommentView.AGREE).map(
+                  (targetComment, index) => (
+                    <CommentCard
+                      onClickLike={() =>
+                        handleLikeChildComment(targetComment.id)
+                      }
+                      key={`agree-comment-${index}`}
+                      comment={targetComment}
+                      onClickAddComment={() => {}}
+                    />
+                  )
+                )}
+              </div>
+              <div className="w-full flex flex-col gap-[16px] items-center">
+                {getCommentsByView(CommentView.PARTIAL_AGREE).map(
+                  (targetComment, index) => (
+                    <CommentCard
+                      onClickLike={() =>
+                        handleLikeChildComment(targetComment.id)
+                      }
+                      key={`partial-agree-comment-${index}`}
+                      comment={targetComment}
+                      onClickAddComment={() => {}}
+                    />
+                  )
+                )}
+              </div>
+              <div className="w-full flex flex-col gap-[16px] items-center">
+                {getCommentsByView(CommentView.DISAGREE).map(
+                  (targetComment, index) => (
+                    <CommentCard
+                      onClickLike={() =>
+                        handleLikeChildComment(targetComment.id)
+                      }
+                      key={`disagree-comment-${index}`}
+                      comment={targetComment}
+                      onClickAddComment={() => {}}
+                    />
+                  )
+                )}
+              </div>
+            </div>
+          </section>
         </div>
-      </section>
+      ) : (
+        <div>404</div>
+      )}
     </div>
   );
 }
---
+++ src/util/converter.ts
@@ -0,0 +1,124 @@
+import { database, get, ref } from "../lib/firebase";
+import { Comment, CommentView, Room } from "../types/room";
+
+export const ConvertCommentView = (text: string): CommentView => {
+  switch (text) {
+    case "เห็นด้วย":
+      return CommentView.AGREE;
+    case "เห็นด้วยบางส่วน":
+      return CommentView.PARTIAL_AGREE;
+    case "ไม่เห็นด้วย":
+      return CommentView.DISAGREE;
+    default:
+      return CommentView.AGREE;
+  }
+};
+
+export const ConvertRoom = async (id: string, data: any): Promise<Room> => {
+  const comments = await Promise.all(
+    (data.comment_ids || []).map(async (commentId: string) => {
+      const com = await getCommentWithoutChildren(commentId);
+      console.log("com => ", com);
+      return com;
+    })
+  );
+  return {
+    id: id as string,
+    title: data.title,
+    comments: comments,
+    created_at: data.created_at,
+    updated_at: data.updated_at,
+  };
+};
+
+export const GetCommentWitChildren = async (
+  id: string
+): Promise<Comment | null> => {
+  const commentRef = ref(database, "comments/" + id);
+  try {
+    const snapshot = await get(commentRef);
+    if (snapshot.exists()) {
+      const data = snapshot.val();
+      console.log(data);
+      return ConvertCommentWithChildren(id, data);
+    } else {
+      console.log("No data available");
+      return null;
+    }
+  } catch (e) {
+    console.error("Error fetching data:", e);
+  }
+  return null;
+};
+
+export const ConvertCommentWithoutChildren = (
+  id: string,
+  data: any
+): Comment => {
+  return {
+    id: id,
+    comment_view: ConvertCommentView(data.comment_view),
+    reason: data.reason,
+    like_count: data.like_count,
+    comments: [], // no need to use
+    parent_comment_ids: data.parent_comment_ids || [],
+    parent_room_id: data.parent_room_id,
+    created_at: data.created_at,
+    updated_at: data.updated_at,
+  };
+};
+
+export const getCommentWithoutChildren = async (
+  id: string
+): Promise<Comment | null> => {
+  const commentRef = ref(database, "comments/" + id);
+  try {
+    const snapshot = await get(commentRef);
+    if (snapshot.exists()) {
+      const data = snapshot.val();
+      console.log(data);
+      return ConvertCommentWithoutChildren(id, data);
+    } else {
+      console.log("No data available");
+      return null;
+    }
+  } catch (e) {
+    console.error("Error fetching data:", e);
+  }
+  return null;
+};
+
+export const ConvertCommentWithChildren = async (
+  id: string,
+  data: any
+): Promise<Comment> => {
+  if (data.comment_ids) {
+    const comments = Promise.all(
+      data.comment_ids.map(async (commentId: string) => {
+        return await getCommentWithoutChildren(commentId);
+      })
+    );
+    return {
+      id: id,
+      comment_view: ConvertCommentView(data.comment_view),
+      reason: data.reason,
+      like_count: data.like_count,
+      comments: await comments,
+      parent_comment_ids: data.parent_comment_ids || [],
+      parent_room_id: data.parent_room_id,
+      created_at: data.created_at,
+      updated_at: data.updated_at,
+    };
+  }
+  return {
+    id: id,
+    comment_view: ConvertCommentView(data.comment_view),
+    reason: data.reason,
+    like_count: data.like_count,
+    comments: data.comments || [],
+    parent_comment_ids: data.parent_comment_ids || [],
+    parent_room_id: data.parent_room_id,
+    created_at: data.created_at,
+    updated_at: data.updated_at,
+  };
+};
